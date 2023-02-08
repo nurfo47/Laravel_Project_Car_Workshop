cee92a7b43181ebe60e889856e3bae206a4089b2
nurfo47 <nurfet.mehulic00@gmail.com>
2023-02-08 20:48:54 +0100

    Added link to page with sql statements on navbar

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -34,6 +34,12 @@
                         {{ __('Dijelovi') }}
                     </x-jet-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('carworker') }}" :active="request()->routeIs('carworker')">
+                        {{ __('Upiti') }}
+                    </x-jet-nav-link>
+                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
