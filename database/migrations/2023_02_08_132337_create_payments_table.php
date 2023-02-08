aabb33539cc887caddb0bb54c2465500cd083965
nurfo47 <nurfet.mehulic00@gmail.com>
2023-02-08 14:32:58 +0100

    Created payments table

---
+++ database/migrations/2023_02_08_132337_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->double('total');
+            $table->integer('carworker_id');
+            $table->integer('costumer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
