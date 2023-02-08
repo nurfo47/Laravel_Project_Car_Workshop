a7f46af2f170b2134f8d97ffffe9008b8cb1b251
nurfo47 <nurfet.mehulic00@gmail.com>
2023-02-08 21:04:00 +0100

    Added comments

--- app/Models/payment.php
+++ app/Models/payment.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use DB;
+//used just for testing, can't sum all prices to one total payment
 
-class payment extends Model
+class Payment extends Model
 {
     public $timestamps=false;
+
     use HasFactory;
 
     protected $fillable=[
