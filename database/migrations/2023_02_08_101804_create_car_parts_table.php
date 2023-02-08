81c6948dd93ca4fdeee9a0506d21b1c5126d4ac1
nurfo47 <nurfet.mehulic00@gmail.com>
2023-02-08 11:24:20 +0100

    Created table for car parts

---
+++ database/migrations/2023_02_08_101804_create_car_parts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_parts', function (Blueprint $table) {
+            $table->id();
+            $table->string('pname');
+            $table->double('price');
+            $table->integer('carworker_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_parts');
+    }
+};
